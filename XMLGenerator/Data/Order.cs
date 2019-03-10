eb8b87169d71e090535768b1f1e7cb44aa3bbf32
arkadiuszgrabowski <37307818+ari0987@users.noreply.github.com>
2019-03-10 21:27:05 +0100

    Init

---
+++ XMLGenerator/Data/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Order
+    {
+        private long Id { get; set; }
+        //Content trzeba zrobić jako listę przedmiotów chyba to bedzie złożony typ danych
+        private String Content { get; set; }
+        private String Address { get; set; }
+        private DateTime SendingDateTime { get; set; }
+        private DateTime EstimatedDelivery { get; set; }
+        private OrderType Type { get; set; }
+    }
+}
